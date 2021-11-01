9a7ebe0d97548096f7898016682f051b68ce40d8
Stuart Jauncey <stuart.jauncey@shift.online>
2021-11-01 10:52:50 +0000

    creates categories and users tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,17 @@
-const seed = (data) => {
+const db = require("../connection");
+
+const seed = async (data) => {
+  //Drop tables if they exist
   const { categoryData, commentData, reviewData, userData } = data;
+  await db.query(`DROP TABLE IF EXISTS categories`);
   // 1. create tables
+  await db
+  .query(`
+    CREATE TABLE categories (
+      slug VARCHAR PRIMARY KEY,
+      description VARCHAR
+    )
+  ;`)
   // 2. insert data
 };
 
